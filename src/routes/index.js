16b203c07017d391b6b9e0cde54d7c2b30a183f0
Shaun Norton <shaunnorton87@gmail.com>
2021-04-21 23:07:46 -0700

    Users can sign UP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import homepage from './homepage.js'
 import posts from './posts.js'
 import comments from "./comments"
+import auth from "./auth"
 
 export default {
     homepage,
     posts,
-    comments
+    comments,
+    auth
 }
--- src/server.js
+++ src/server.js
@@ -1,24 +1,29 @@
+require("dotenv").config()
 import express from "express"
 import routes from './routes/index.js'
 import handlebars from 'express-handlebars'
 import bodyParser from 'body-parser'
 import expressValidator from 'express-validator'
 require("./data/reddit-db")
+import cookieParser from "cookie-parser"
+import jwt from "jsonwebtoken"
+
 
 const app = express()
 app.use(express.static("./src/public"))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(expressValidator())
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set("view engine", 'handlebars')
 app.set("views", "./src/views")
-
+app.use(cookieParser())
 
 app.use('/',routes.homepage)
 app.use('/',routes.posts)
 app.use('/',routes.comments)
+app.use('/',routes.auth)
 // require('./controllers/posts.js')(app)
 
 
