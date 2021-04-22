b4ffde1ca8df1b058ddc29711469a7b75c886630
Shaun Norton <shaunnorton87@gmail.com>
2021-04-21 23:24:54 -0700

    Users can log in

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -30,4 +30,32 @@ router.get("/logout", (req ,res) => {
     res.redirect("/")
 })
 
+router.get('/login', (req,res) => {
+    res.render("login")
+})
+
+router.post('/login', (req,res) => {
+    const username = req.body.username
+    const password = req.body.password
+    User.findOne({username}, "username password")
+        .then( user => {
+            if(!user) {
+                return  res.status(401).send({message:"Wrong Username or Password"})
+            }
+            user.comparePassword(password, (err, isMatch) => {
+                if (!isMatch) {
+                    return  res.status(401).send({message:"Wrong Username or Password"})
+                }
+                const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                    expiresIn: "60 days"
+                })
+                res.cookie("nToken", token, { maxAge: 900000, httpOnly: true })
+                res.redirect("/")
+            })  
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
 export default router
